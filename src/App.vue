a9216300cd900fe5787ecd5084d704dcfda0ee31
Oleh Fostyak <o.fostyak@softonix.org>
2024-08-14 18:01:20 +0300

    (section-1): Updated readme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="p-4 text-black">Hello World!</div>
+  <div class="p-4 text-black text-black">Hello {{ data.name }}!
+  </div>
 </template>
 
 <script lang="ts" setup>
-// YOUR CODE HERE
+import { eslintExample } from '@/examples/eslint-example'
+
+const data = reactive({ name: 'Player' });
+
+onMounted(() => {
+  eslintExample.renderList()
+})
 </script>
